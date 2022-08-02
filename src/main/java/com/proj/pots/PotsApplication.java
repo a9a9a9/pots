016f8007efe7d4e5908851daa57fbcae9e92460d
hyegyu <lhg2008@naver.com>
2022-08-02 17:04:39 +0900

    co

--- src/main/java/com/proj/pots/PotsApplication.java
+++ src/main/java/com/proj/pots/PotsApplication.java
@@ -2,12 +2,12 @@ package com.proj.pots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PotsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PotsApplication.class, args);
 	}
-
 }
