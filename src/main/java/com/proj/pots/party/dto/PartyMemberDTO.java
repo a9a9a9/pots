c64ab150d0bdf618e84c1d2350894f9c47ff4dce
hyegyu <lhg2008@naver.com>
2022-08-04 20:10:17 +0900

    DTO

--- src/main/java/com/proj/pots/party/dto/PartyMemberDTO.java
+++ src/main/java/com/proj/pots/party/dto/PartyMemberDTO.java
@@ -1,31 +1,10 @@
 package com.proj.pots.party.dto;
 
-public class PartyMemberDTO {
+public class PartyMemberDTO extends PartyDTO{
 
-	private String id;			//아이디(이메일)
-	private String nick;		//닉네임
-	private int party_num;		//파티번호
 	private int mycharge;		//결제한 금액
 	private String mystartday;	//파티 가입일
 	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getNick() {
-		return nick;
-	}
-	public void setNick(String nick) {
-		this.nick = nick;
-	}
-	public int getParty_num() {
-		return party_num;
-	}
-	public void setParty_num(int party_num) {
-		this.party_num = party_num;
-	}
 	public int getMycharge() {
 		return mycharge;
 	}
