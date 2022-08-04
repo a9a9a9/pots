12d7bf753b404f607c14c062e487ee2df04cfdc4
CW <bcu0709@naver.com>
2022-08-04 18:17:38 +0900

    point

--- src/main/java/com/proj/pots/HomeController.java
+++ src/main/java/com/proj/pots/HomeController.java
@@ -143,4 +143,12 @@ public class HomeController {
 	public String faq4() {
 		return "myMenu/faq4";
 	}
+	@RequestMapping(value = "myPointCharge")
+	public String myPointCharge() {
+		return "myMenu/myPointCharge";
+	}
+	@RequestMapping(value = "myPoint")
+	public String myPoint() {
+		return "myMenu/myPoint";
+	}
 }
