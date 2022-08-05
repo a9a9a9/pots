5d35f7092db472b636937046558e518581b8df63
CW <bcu0709@naver.com>
2022-08-05 20:33:55 +0900

    db

--- src/main/java/com/proj/pots/membership/MemberController.java
+++ src/main/java/com/proj/pots/membership/MemberController.java
@@ -2,10 +2,14 @@ package com.proj.pots.membership;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.proj.pots.member.dto.MemberDTO;
 import com.proj.pots.membership.service.MemberService;
 
 @Controller
@@ -17,5 +21,18 @@ public class MemberController {
 	public String isExistId(@RequestBody(required = false) String id) {
 		String msg = memberService.isExistId(id);
 		return msg;
+	}
+	
+	@RequestMapping(value = "memberProc")
+	public String memberProc(MemberDTO member, Model model, RedirectAttributes ra) {
+		String msg = memberService.memberProc(member);
+		
+		if(msg.equals("가입 완료")) {
+			ra.addFlashAttribute("msg",msg);
+			return "redirect:/index?formpath=home";
+		}else {
+			model.addAttribute("msg", msg);
+			return "forward:/index?formpath=register";
+		}
 	}
 }
--- src/main/java/com/proj/pots/membership/service/MemberService.java
+++ src/main/java/com/proj/pots/membership/service/MemberService.java
@@ -3,8 +3,10 @@ package com.proj.pots.membership.service;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.proj.pots.member.dto.MemberDTO;
 import com.proj.pots.membership.dao.IMemberDAO;
 
 @Service
@@ -19,5 +21,22 @@ public class MemberService {
 		if(count == 1)
 			return "중복 아이디 입니다.";
 		return "사용 가능한 아이디입니다.";
+	}
+	
+	public String memberProc(MemberDTO member) {
+		
+		if(member.getId() == null || member.getId().isEmpty())
+			return "아이디를 입력하세요.";
+		if(member.getPw() == null || member.getPw().isEmpty())
+			return "비밀번호를 입력하세요.";
+		if(memberDao.isExistId(member.getId()) > 0)
+			return "중복 아이디 입니다.";
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String securePw = encoder.encode(member.getPw());
+		member.setPw(securePw);
+		
+		memberDao.insertMember(member);
+		return "가입 완료";
 	}
 }
