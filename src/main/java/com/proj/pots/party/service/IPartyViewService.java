f38db851e8a3edb4fa162d4966aaf0970ed1ae45
lhg1042 <65995264+lhg1042@users.noreply.github.com>
2022-09-29 15:39:54 +0900

    Update IPartyViewService.java

--- src/main/java/com/proj/pots/party/service/IPartyViewService.java
+++ src/main/java/com/proj/pots/party/service/IPartyViewService.java
@@ -11,33 +11,33 @@ import com.proj.pots.party.dto.PartyMemberDTO;
 import com.proj.pots.party.dto.PartyRegDTO;
 
 public interface IPartyViewService { 
-	
-	public ArrayList<PartyCommentDTO> comment(String id); 
-	
-	public MemberDTO memberInfo(String id); 
- 
-	PartnerInfoDTO selectAccount(String id);
-		
+
 	PartyRegDTO selectParty(String party_num);
+	
+	public String partyMemberInsertProc(PartyMemberDTO partyMember, String party_num);
+	
+	ArrayList<PartyListDTO> createdParty(String id);
+	
+	ArrayList<PartyListDTO> joinedParty(String id);
 	
 	PartyMemberDTO partyMember(String id);
 	
 	PartyRegDTO partyDay(String party_num);
 	
 	PartyMemberDTO myPartyDay(String id, String party_num);
-	 
-	PartyMemberDTO payMethod(PartyDTO my); 
-		
+	
+	ArrayList<PartyCommentDTO> comment(String id); 
+
 	public String accountInsertProc(PartnerInfoDTO partner);
 
 	public String accountModifyProc(PartnerInfoDTO partner);
 	
-	ArrayList<PartyListDTO> createdParty(String id);
-	
-	ArrayList<PartyListDTO> joinedParty(String id);
+	PartnerInfoDTO selectAccount(String id);
+	 
+	PartyMemberDTO payMethod(PartyDTO my); 
 
 	ArrayList<PartyMemberDTO> orderList(String id);
 
-	public String partyMemberInsertProc(PartyMemberDTO partyMember, String party_num);
+	public MemberDTO memberInfo(String id); 
 	
 }
